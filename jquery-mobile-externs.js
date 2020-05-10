b4ef9c1dea5cfac852db99cf92146e996f194036
idotj <info@ipuntoj.com>
2020-05-10 17:10:14 +0200

    remove unused files + update .gitignore

--- jquery-mobile-externs.js
+++
@@ -1,6 +0,0 @@
-$.listview = function(arg1) {}
-$.popup = function(arg1) {}
-$.collapsibleset = function(arg1) {}
-$.checkboxradio = function(arg1) {}
-$.mobile.loading = function(arg1) {}
-$.textinput = function(arg1) {}
