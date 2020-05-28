74e61df1742fbaad311108daf13ae9ed1a51d54e
idotj <info@ipuntoj.com>
2020-05-28 18:56:11 +0200

    improve Games list + responsive

--- inc/gui/lobby/lobby_gamelist.js
+++ inc/gui/lobby/lobby_gamelist.js
@@ -21,44 +21,62 @@ function LobbyGameListImpl()
 	this.addGame = function(gameId) 
 	{
 		var gameTypeGfxUrl;
+		var gameTypeGfxTitle;
 		switch (myNetCache.getGameData(gameId).gameInfo.netGameType) 
 		{
-			case 1: gameTypeGfxUrl = "gfx/lobby/gametype_standard.png";
-			break;
-			case 2: gameTypeGfxUrl = "gfx/lobby/gametype_registered.png";
-			break;
-			case 3: gameTypeGfxUrl = "gfx/lobby/gametype_inviteonly.png";
-			break;
-			case 4: gameTypeGfxUrl = "gfx/lobby/gametype_ranking.png";
-			break;
-			default:;
+			case 1:
+				gameTypeGfxUrl = "gfx/lobby/gametype_standard.png";
+				gameTypeGfxTitle = 'Standard game';
+				break;
+			case 2:
+				gameTypeGfxUrl = "gfx/lobby/gametype_registered.png";
+				gameTypeGfxTitle = 'Registered game';
+				break;
+			case 3:
+				gameTypeGfxUrl = "gfx/lobby/gametype_inviteonly.png";
+				gameTypeGfxTitle = 'Invite only game';
+				break;
+			case 4:
+				gameTypeGfxUrl = "gfx/lobby/gametype_ranking.png";
+				gameTypeGfxTitle = 'Ranking game';
+				break;
+			default:
 		}
 
 		var gamePrivateGfxUrl;
+		var gamePrivateGfxTitle;
 		if(myNetCache.getGameData(gameId).isPrivate) {
-			gamePrivateGfxUrl = "gfx/lobby/gameprivate_locked.png";      
+			gamePrivateGfxUrl = "gfx/lobby/gameprivate_locked.png"; 
+			gamePrivateGfxTitle = 'Private game'     
 		}
 		else {
-			gamePrivateGfxUrl = "gfx/1px.png";
+			gamePrivateGfxUrl = '';
 		}
 
 		var gameModeGfxUrl;
+		var gameModeGfxTitle;
 		if(myNetCache.getGameData(gameId).gameMode == 1) {
-			gameModeGfxUrl = "gfx/lobby/open_game.png";      
+			gameModeGfxUrl = "gfx/lobby/open_game.png";
+			gameModeGfxTitle = 'Open game';    
 		}
 		else if(myNetCache.getGameData(gameId).gameMode == 2) {
 			gameModeGfxUrl = "gfx/lobby/running_game.png";
+			gameModeGfxTitle = 'Running game';
 		}
 		else {
-			gameModeGfxUrl = "gfx/1px.png";
+			gameModeGfxUrl = '';
+			gameModeGfxTitle = '';
 		}
 
 		var gameAllowSpectatorsGfxUrl;
+		var gameAllowSpectatorsGfxTitle;
 		if(myNetCache.getGameData(gameId).gameInfo.allowSpectators) {
 			gameAllowSpectatorsGfxUrl = "gfx/lobby/spectator.png";
+			gameAllowSpectatorsGfxTitle = 'Allow spectators'
 		}
 		else {
 			gameAllowSpectatorsGfxUrl = "gfx/lobby/no_spectator.png";
+			gameAllowSpectatorsGfxTitle = 'No spectators allowed'
 		}
 
 		var playerString = "";
@@ -109,7 +127,7 @@ function LobbyGameListImpl()
 		}
 		//COLLAPSIBLE TEST
 		$('#gameListCollapsibleSet').append('<div class="gameListCollapsible" id="gameListGameID-'+gameId+'" data-role="collapsible"> '+
-			'<h2><div id="lobbyGameList_gameDesc" class="lobbyGameList_gameDesc">'+myNetCache.getGameData(gameId).gameInfo.gameName+'</div><div class="lobbyGameList_numberOfPlayers" id="lobbyGameList_numberOfPlayers_gameId-'+gameId+'">'+playerCounter+'/'+myNetCache.getGameData(gameId).gameInfo.maxNumPlayers+'</div><div class="lobbyGameList_gameMode" id="lobbyGameList_gameMode_gameId-'+gameId+'"><img src=\"'+gameModeGfxUrl+'\" width=25 height=25></div><div id="lobbyGameList_gameType" class="lobbyGameList_gameType"><img src=\"'+gameTypeGfxUrl+'\" height=25></div><div id="lobbyGameList_gamePrivate" class="lobbyGameList_gamePrivate"><img src=\"'+gamePrivateGfxUrl+'\" width=20 height=24></div><div id="lobbyGameList_gameSpectators" class="lobbyGameList_gameSpectators"><img id="lobbyGameList_gameSpectators_img" src=\"'+gameAllowSpectatorsGfxUrl+'\" width=26 height=26><span class="lobbyGameList_gameNumberOfSpectators" id="lobbyGameList_gameNumberOfSpectators_gameId-'+gameId+'">'+numberOfSpectators+'</span></div><div id="lobbyGameList_gameTimeouts" class="lobbyGameList_gameTimeouts">'+myNetCache.getGameData(gameId).gameInfo.playerActionTimeout+'s/'+myNetCache.getGameData(gameId).gameInfo.delayBetweenHands+'s</div></h2> '+
+			'<h2><div id="lobbyGameList_gameDesc" class="lobbyGameList_gameDesc">'+myNetCache.getGameData(gameId).gameInfo.gameName+'</div><div class="lobbyGameList_numberOfPlayers" id="lobbyGameList_numberOfPlayers_gameId-'+gameId+'">'+playerCounter+'/'+myNetCache.getGameData(gameId).gameInfo.maxNumPlayers+'</div><div class="lobbyGameList_gameMode" id="lobbyGameList_gameMode_gameId-'+gameId+'"><img src=\"'+gameModeGfxUrl+'\" title=\"'+gameModeGfxTitle+'\"></div><div id="lobbyGameList_gameType" class="lobbyGameList_gameType"><img src=\"'+gameTypeGfxUrl+'\" title=\"'+gameTypeGfxTitle+'\"></div><div id="lobbyGameList_gamePrivate" class="lobbyGameList_gamePrivate"><img src=\"'+gamePrivateGfxUrl+'\" title=\"'+gamePrivateGfxTitle+'\"></div><div id="lobbyGameList_gameSpectators" class="lobbyGameList_gameSpectators"><img id="lobbyGameList_gameSpectators_img" src=\"'+gameAllowSpectatorsGfxUrl+'\" title=\"'+gameAllowSpectatorsGfxTitle+'\"><span class="lobbyGameList_gameNumberOfSpectators" id="lobbyGameList_gameNumberOfSpectators_gameId-'+gameId+'">'+numberOfSpectators+'</span></div><div id="lobbyGameList_gameTimeouts" class="lobbyGameList_gameTimeouts">'+myNetCache.getGameData(gameId).gameInfo.playerActionTimeout+'s/'+myNetCache.getGameData(gameId).gameInfo.delayBetweenHands+'s</div></h2> '+
 				'<ul id="lobbyGameList_gameDetails_gameId-'+gameId+'" data-role="listview" data-theme="d" data-divider-theme="d"> '+
                                     playerString +
                                     '<li> '+
@@ -151,17 +169,21 @@ function LobbyGameListImpl()
 	this.updateGame = function(gameId) 
 	{
 		var gameModeGfxUrl;
+		var gameModeGfxTitle;
 		if(myNetCache.getGameData(gameId).gameMode == 1) {
-			gameModeGfxUrl = "gfx/lobby/open_game.png";      
+			gameModeGfxUrl = "gfx/lobby/open_game.png";  
+			gameModeGfxTitle = 'Open game';
 		}
 		else if(myNetCache.getGameData(gameId).gameMode == 2) {
 			gameModeGfxUrl = "gfx/lobby/running_game.png";
+			gameModeGfxTitle = 'Running game'
 		}
 		else {
-			gameModeGfxUrl = "gfx/1px.png";
+			gameModeGfxUrl = '';
+			gameModeGfxTitle = '';  
 		}
 
-		$("#lobbyGameList_gameMode_gameId-"+gameId).html('<img src=\"'+gameModeGfxUrl+'\" width=25 height=25>');
+		$("#lobbyGameList_gameMode_gameId-"+gameId).html('<img src=\"'+gameModeGfxUrl+'\" title=\"'+gameModeGfxTitle+'\">');
 		$("#lobbyGameList_gameNumberOfSpectators_gameId-"+gameId).html(myNetCache.getGameData(gameId).spectatorIds.length);
 	};
 
